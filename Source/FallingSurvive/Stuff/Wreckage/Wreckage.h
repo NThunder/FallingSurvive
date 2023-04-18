ca2dca287b7e0fffdc4c3919f83d828abf8b5bf5
Григорий Бухтуев <bukhtuev.ga@phystech.edu>
2023-04-18 15:12:22 +0300

    mvp version 0.1

--- Source/FallingSurvive/Stuff/Wreckage/Wreckage.h
+++ Source/FallingSurvive/Stuff/Wreckage/Wreckage.h
@@ -1,23 +1,17 @@
-﻿#pragma once
+﻿
+
+#pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "../Staff.h"
 #include "Wreckage.generated.h"
 
+/**
+ *
+ */
 UCLASS()
-class FALLINGSURVIVE_API AWreckage : public AActor
+class FALLINGSURVIVE_API AWreckage : public AStaff
 {
 	GENERATED_BODY()
-	
-public:
-	// Устанавливает значения по умолчанию для свойств этого объекта actor
-	AWreckage();
 
-protected:
-	// Вызывается при запуске игры или при возрождении
-	virtual void BeginPlay() override;
-
-public:
-	// Вызывается каждый кадр
-	virtual void Tick(float DeltaTime) override;
 };
